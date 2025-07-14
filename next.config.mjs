f447d2fc77f904a2f8e475b42ea6f9063f621a40
Muskan kumari <122370605+assistanceshiri12225@users.noreply.github.com>
2025-07-14 19:55:58 +0530

    Fix: added pathname for remote image support

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "randomuser.me",
-    },
-  ],
-},
-
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**", // ✅ Add this line
+      },
+    ],
+  },
   experimental: {
     allowedDevOrigins: ["http://localhost:3000", "http://192.168.56.1:3000"],
   },

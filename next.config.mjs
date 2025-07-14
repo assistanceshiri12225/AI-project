cd7d3936969717d0c1f5622fd05c121a59a4bbc2
Muskan kumari <122370605+assistanceshiri12225@users.noreply.github.com>
2025-07-14 20:09:17 +0530

    Fix image issue by adding pathname in config

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,9 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "randomuser.me",
-        pathname: "/api/portraits/**", // ✅ Add this line
+        pathname: "/api/portraits/**", // ✅ Yeh line bahut zaroori hai
       },
     ],
-  },
-  experimental: {
-    allowedDevOrigins: ["http://localhost:3000", "http://192.168.56.1:3000"],
   },
 };
 

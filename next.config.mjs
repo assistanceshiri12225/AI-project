bcae045765137898853d1ebe8c5a1428b2911966
Muskan kumari <122370605+assistanceshiri12225@users.noreply.github.com>
2025-07-14 18:12:03 +0530

    Initial commit or config update for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "randomuser.me",
+    },
+  ],
+},
+
   experimental: {
     allowedDevOrigins: ["http://localhost:3000", "http://192.168.56.1:3000"],
   },

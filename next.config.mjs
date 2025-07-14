e7f5480e6ffd1a66f75a6697dc0e1e14dcad49dc
Muskan kumari <122370605+assistanceshiri12225@users.noreply.github.com>
2025-07-14 20:15:39 +0530

    Fix: adjusted Image props for correct display on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/**", // ✅ Yeh line bahut zaroori hai
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/api/portraits/**',
       },
     ],
   },
